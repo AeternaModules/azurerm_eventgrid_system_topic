b679daefd60432c126d76eb5e7264983f5e4ccff
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:38 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,44 @@ output "eventgrid_system_topics" {
   description = "All eventgrid_system_topic resources"
   value       = azurerm_eventgrid_system_topic.eventgrid_system_topics
 }
+output "eventgrid_system_topics_identity" {
+  description = "List of identity values across all eventgrid_system_topics"
+  value       = [for k, v in azurerm_eventgrid_system_topic.eventgrid_system_topics : v.identity]
+}
+output "eventgrid_system_topics_location" {
+  description = "List of location values across all eventgrid_system_topics"
+  value       = [for k, v in azurerm_eventgrid_system_topic.eventgrid_system_topics : v.location]
+}
+output "eventgrid_system_topics_metric_arm_resource_id" {
+  description = "List of metric_arm_resource_id values across all eventgrid_system_topics"
+  value       = [for k, v in azurerm_eventgrid_system_topic.eventgrid_system_topics : v.metric_arm_resource_id]
+}
+output "eventgrid_system_topics_metric_resource_id" {
+  description = "List of metric_resource_id values across all eventgrid_system_topics"
+  value       = [for k, v in azurerm_eventgrid_system_topic.eventgrid_system_topics : v.metric_resource_id]
+}
+output "eventgrid_system_topics_name" {
+  description = "List of name values across all eventgrid_system_topics"
+  value       = [for k, v in azurerm_eventgrid_system_topic.eventgrid_system_topics : v.name]
+}
+output "eventgrid_system_topics_resource_group_name" {
+  description = "List of resource_group_name values across all eventgrid_system_topics"
+  value       = [for k, v in azurerm_eventgrid_system_topic.eventgrid_system_topics : v.resource_group_name]
+}
+output "eventgrid_system_topics_source_arm_resource_id" {
+  description = "List of source_arm_resource_id values across all eventgrid_system_topics"
+  value       = [for k, v in azurerm_eventgrid_system_topic.eventgrid_system_topics : v.source_arm_resource_id]
+}
+output "eventgrid_system_topics_source_resource_id" {
+  description = "List of source_resource_id values across all eventgrid_system_topics"
+  value       = [for k, v in azurerm_eventgrid_system_topic.eventgrid_system_topics : v.source_resource_id]
+}
+output "eventgrid_system_topics_tags" {
+  description = "List of tags values across all eventgrid_system_topics"
+  value       = [for k, v in azurerm_eventgrid_system_topic.eventgrid_system_topics : v.tags]
+}
+output "eventgrid_system_topics_topic_type" {
+  description = "List of topic_type values across all eventgrid_system_topics"
+  value       = [for k, v in azurerm_eventgrid_system_topic.eventgrid_system_topics : v.topic_type]
+}
 
